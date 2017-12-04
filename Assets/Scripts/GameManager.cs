d6a43c6600584340efd86b117367c6b958581770
Noah Ratcliff <me@nratcliff.com>
2017-12-03 19:36:12 -0500

    game manager, title

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+    public GameObject CollectTitle, PaintTitle;
+    public KatamariBall Ball;
+    public SizeMeter SizeMeter;
+    public float CollectTitleDelay;
+
+    private float _collectTitleTime;
+    
+    private void Start()
+    {
+        //Ball.OnPickup.AddListener(_onPickup);
+        SizeMeter.OnGoalReached.AddListener(_onGoalReached);
+        _collectTitleTime = CollectTitleDelay;
+    }
+
+    //private void _onPickup()
+    //{
+
+    //}
+    private void Update()
+    {
+        if (!CollectTitle.activeSelf && _collectTitleTime > 0)
+            _collectTitleTime -= Time.deltaTime;
+        else if (!CollectTitle.activeSelf)
+        {
+            CollectTitle.SetActive(true);
+            //todo: sound
+        }
+
+    }
+
+    private void _onGoalReached()
+    {
+        PaintTitle.SetActive(true);
+        // todo: sound
+    }
+}
---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour 
+{
+    public AudioSource A, B;
+    public float fadeSpeed;
+
+    private bool _fading = false;
+    private float _t = 0;
+
+    // fade from A to B
+    public void CrossfadeTracks()
+    {
+        if (_t >= 1) return;
+
+        _fading = true;
+    }
+
+    private void Update()
+    {
+        if (!_fading) return;
+
+        _t += fadeSpeed * Time.deltaTime;
+
+        A.volume = Mathf.Lerp(1, 0, Mathf.Lerp(0, 1, _t));
+        B.volume = Mathf.Lerp(0, 1, Mathf.Lerp(0, 1, _t));
+
+        if (_t >= 1) _fading = false;
+    }
+}
